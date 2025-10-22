7bbf9696c95d8effc1383f838d7144ca61d1e50d
Nicolas O <noccis@I7413>
2025-10-22 21:57:01 +0200

    Propose 2 versions for Project Euler 1: traditional and modern (with cl-lib)

--- project-euler-001.el
+++ project-euler-001.el
@@ -1,14 +1,18 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun project-euler-1 (n)
+(defun project-euler-1 (&optional lim)
+  "Solve Project Euler 1"
+  (cl-loop for n from 0 below (or lim 1000)
+	   when (or (= 0 (mod n 3)) (= 0 (mod n 5)))
+	   sum n))
+
+(defun project-euler-1--traditional ()
   "Solve Project Euler 1."
-  (let ((res 0))
+  (let ((n 1000)
+        (res 0))
     (dotimes (i n)
       (when (or (= 0 (mod i 3)) (= 0 (mod i 5)))
         (setq res (+ res i))))
     res))
-
-(project-euler-1 10) ; 23
-(project-euler-1 1000)
 
 ;;; end
