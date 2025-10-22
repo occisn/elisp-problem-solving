803b80a0a33804aebc241834c6489c9e376365a1
Nicolas O <noccis@I7413>
2025-10-22 21:57:53 +0200

    Propose 2 versions for Project Euler 6: traditional and modern (with cl-lib)

--- project-euler-006.el
+++ project-euler-006.el
@@ -1,16 +1,20 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun project-euler-6 (n)
+(defun project-euler-6 (&optional lim)
+  (cl-loop for i from 1 to (or lim 100)
+	   sum i into sum1
+	   sum (* i i) into sum2
+	   finally return (- (* sum1 sum1) sum2)))
+
+(defun project-euler-6--traditional ()
   "Solve Project Euler 6."
-  (let ((res 0))
+  (let ((n 100)
+        (res 0))
     (dotimes (i (+ n 1))
       (setq res (+ res i)))
     (setq res (* res res))
     (dotimes (i (+ n 1))
       (setq res (- res (* i i))))
     res))
-
-(project-euler-6 10) ; 2640
-(project-euler-6 100); 25164150
 
 ;;; end
