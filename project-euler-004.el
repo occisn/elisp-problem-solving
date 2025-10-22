dd7faedc6b51eea9bb9e6a9175d662e9a8e59c98
Nicolas O <noccis@I7413>
2025-10-22 21:44:35 +0200

    Fix details

--- project-euler-004.el
+++ project-euler-004.el
@@ -1,63 +1,46 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun project-euler-4a ()
-  "Solve Project Euler 4.
-Emacs Lisp with cl-lib"
-
-  (cl-labels ((reverse-number (n &optional acc0)
-                "Reverse the N, which is supposed to be an integer >= 0.
+(defun my/reverse-number (n &optional acc0)
+  "Reverse the N, which is supposed to be an integer >= 0.
 For instance: 123 --> 321.
 ACC0 is an accumulator used during recursion.
 (v1, available in occisn/elisp-utils GitHub repository)"
-                (let ((acc (or acc0 0)))
-                  (if (= n 0)
-	              acc
-                    (let ((f (floor n 10))
-	                  (r (mod n 10)))
-	              (reverse-number f (+ (* 10 acc) r)))))))
-    
-    (cl-loop for a from 100 to 998
-	     for c = (cl-loop for b from a to 999
-			      for prod = (* a b)
-			      when (= prod (reverse-number prod))
-			      maximize prod)
-	     when (not (null c))
-	     maximize c)))
+  (let ((acc (or acc0 0)))
+    (if (= n 0)
+	acc
+      (let ((f (floor n 10))
+	    (r (mod n 10)))
+	(my/reverse-number f (+ (* 10 acc) r))))))
 
-(defun project-euler-4b ()
+(defun project-euler-4 ()
+  "Solve Project Euler 4.
+Emacs Lisp with cl-lib"
+
+  (cl-loop for a from 100 to 998
+	   for c = (cl-loop for b from a to 999
+			    for prod = (* a b)
+			    when (= prod (my/reverse-number prod))
+			    maximize prod)
+	   when (not (null c))
+	   maximize c))
+
+(defun project-euler-4--traditional ()
   "Solve Project Euler 4. 
 Traditional Emacs Lisp without cl-lib"
   
-  (letrec (
-           ;; 'reverse-number'
-           ;; Reverse the N, which is supposed to be an integer >= 0.
-           ;; For instance: 123 --> 321.
-           ;; ACC0 is an accumulator used during recursion.
-           ;; (v1, available in occisn/elisp-utils GitHub repository)
-           (sub-reverse-number (lambda (n acc0)
-                                 
-                                 (let ((acc (or acc0 0)))
-                                   (if (= n 0)
-	                               acc
-                                     (let ((f (floor n 10))
-	                                   (r (mod n 10)))
-	                               (funcall sub-reverse-number f (+ (* 10 acc) r)))))))
-           (reverse-number (lambda (n)
-                             (funcall sub-reverse-number n 0))))
-    
-    (let ((a 100)
-          (c-max 0))
-      (while (<= a 998)
-        (let ((b a)
-              (c -1))
-          (while (<= b 999)
-            (let ((prod (* a b)))
-              (when (and (= prod (funcall reverse-number prod)) (> prod c))
-                (setq c prod)))
-            (setq b (1+ b)))
-          (when (> c c-max)
-            (setq c-max c)))
-        (setq a (1+ a)))
-      c-max)))
+  (let ((a 100)
+        (c-max 0))
+    (while (<= a 998)
+      (let ((b a)
+            (c -1))
+        (while (<= b 999)
+          (let ((prod (* a b)))
+            (when (and (= prod (my/reverse-number prod)) (> prod c))
+              (setq c prod)))
+          (setq b (1+ b)))
+        (when (> c c-max)
+          (setq c-max c)))
+      (setq a (1+ a)))
+    c-max))
 
 ;;; end
--- project-euler-007.el
+++ project-euler-007.el
@@ -1,75 +1,78 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun project-euler-7a (&optional target0)
-  "Solve Project Euler 7."
-  (cl-labels ((primep (n)
-                "Return t if and only if N is prime. N is supposed to be an integer >= 1.
+(defun my/primep (n)
+  "Return t if and only if N is prime. N is supposed to be an integer >= 1.
 Inspired by https://github.com/tkych/cl-mod-prime
 (v1, available in occisn/elisp-utils GitHub repository)"
-                (cond ((= 1 n) nil)
-	              ((member n '(2 3 5 7)) t)
-	              ((cl-evenp n) nil)
-	              ((zerop (mod n 3)) nil)
-	              (t (cl-loop for factor from 5 by 6
-		                  with root-n = (cl-isqrt n)
-		                  while (<= factor root-n)
-		                  never (or (zerop (mod n factor))
-			                    (zerop (mod n (+ factor 2)))))))))
-    (let ((target (or target0 10001)))
-      (cl-loop for n from 2 
-	       when (primep n)
-	       count t into counter
-	       until (= counter target)
-	       finally return n))))
+  (cond ((= 1 n) nil)
+	((member n '(2 3 5 7)) t)
+	((cl-evenp n) nil)
+	((zerop (mod n 3)) nil)
+	(t (cl-loop for factor from 5 by 6
+		    with root-n = (cl-isqrt n)
+		    while (<= factor root-n)
+		    never (or (zerop (mod n factor))
+			      (zerop (mod n (+ factor 2))))))))
 
-(defun project-euler-7b ()
-  "Solve Project Euler 7.
-Traditional Emacs Lisp, without cl-lib"
+(defun project-euler-7 (&optional target0)
+  "Solve Project Euler 7."
+  (let ((target (or target0 10001)))
+    (cl-loop for n from 2 
+	     when (my/primep n)
+	     count t into counter
+	     until (= counter target)
+	     finally return n)))
 
-  (let* ((isqrt (lambda (n)
-                  "Return the integer square root of N (largest integer <= sqrt(N)).
+(defun my/isqrt--traditional (n)
+  "Return the integer square root of N (largest integer <= sqrt(N)).
 N is supposed to be >= 0.
 This code is written in traditional Emacs Lisp, without cl-lib (where the equivalent exists: cl-isqrt).
 (v1, available in occisn/elisp-utils GitHub repository)"
-                  (cond
-                   ((= n 0) 0)
-                   ((< n 4) 1)
-                   (t
-                    ;; Newton's method
-                    (let* ((x n)
-                           (y (/ (+ x (/ n x)) 2)))
-                      (while (< y x)
-                        (setq x y)
-                        (setq y (/ (+ x (/ n x)) 2)))
-                      x)))))
-         (primep (lambda (n)
-                   "Return t if and only if N is prime. N is supposed to be an integer >= 1.
+  (cond
+   ((= n 0) 0)
+   ((< n 4) 1)
+   (t
+    ;; Newton's method
+    (let* ((x n)
+           (y (/ (+ x (/ n x)) 2)))
+      (while (< y x)
+        (setq x y)
+        (setq y (/ (+ x (/ n x)) 2)))
+      x))))
+
+(defun my/primep--traditional (n)
+  "Return t if and only if N is prime. N is supposed to be an integer >= 1.
 Inspired by https://github.com/tkych/cl-mod-prime.
 This code is written in traditional Emacs Lisp, without cl-lib.
+Requires my/isqrt--traditional.
 (v1, available in occisn/elisp-utils GitHub repository)"
-                   (cond ((= 1 n) nil)
-	                 ((member n '(2 3 5 7)) t)
-	                 ((zerop (mod n 2)) nil)
-	                 ((zerop (mod n 3)) nil)
-	                 (t (let ((factor 5)
-                                  (root-n (funcall isqrt n))
-                                  (result t))
-                              (while (<= factor root-n)
-                                (when (or (zerop (mod n factor))
-			                  (zerop (mod n (+ factor 2))))
-                                  (setq result nil))
-                                (setq factor (+ factor 6)))
-                              result))))))
+  
+  (cond ((= 1 n) nil)
+	((member n '(2 3 5 7)) t)
+	((zerop (mod n 2)) nil)
+	((zerop (mod n 3)) nil)
+	(t (let ((factor 5)
+                 (root-n (my/isqrt--traditional n))
+                 (result t))
+             (while (<= factor root-n)
+               (when (or (zerop (mod n factor))
+			 (zerop (mod n (+ factor 2))))
+                 (setq result nil))
+               (setq factor (+ factor 6))) ; end of while
+             result))))
 
-    (let ((n 2)
-          (counter 1) ; 2 is first prime
-          (continue t))
-      (while continue
-        (setq n (1+ n))
-        (when (funcall primep n)
-          (setq counter (1+ counter))
-          (when (= counter 10001)
-            (setq continue nil))))
-      n)))
+(defun project-euler-7--traditional ()
+  "Solve Project Euler 7.
+Traditional Emacs Lisp, without cl-lib"
+  (let ((n 2)
+        (counter 1) ; 2 is first prime
+        (continue t))
+    (while continue
+      (setq n (1+ n))
+      (when (my/primep--traditional n)
+        (setq counter (1+ counter))
+        (when (= counter 10001)
+          (setq continue nil))))
+    n))
 
 ;;; end
