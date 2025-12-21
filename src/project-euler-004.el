709ccad7b3095fff3cbe6baec4fd7820e4933a80
Nicolas O <noccis@I7413>
2025-12-21 21:55:27 +0100

    Improve algorithm for Project Euler 4

--- src/project-euler-004.el
+++ src/project-euler-004.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
-(defun my/reverse-number (n &optional acc0)
+(defun pe004--reverse-number (n &optional acc0)
   "Reverse the N, which is supposed to be an integer >= 0.
 For instance: 123 --> 321.
 ACC0 is an accumulator used during recursion.
@@ -10,37 +10,36 @@ ACC0 is an accumulator used during recursion.
 	acc
       (let ((f (floor n 10))
 	    (r (mod n 10)))
-	(my/reverse-number f (+ (* 10 acc) r))))))
+	(pe004--reverse-number f (+ (* 10 acc) r))))))
 
 (defun project-euler-4 ()
   "Solve Project Euler 4.
 Emacs Lisp with cl-lib"
-
-  (cl-loop for a from 100 to 998
-	   for c = (cl-loop for b from a to 999
-			    for prod = (* a b)
-			    when (= prod (my/reverse-number prod))
-			    maximize prod)
-	   when (not (null c))
-	   maximize c))
+  (cl-loop with max-palindrome = 0
+           for i from 999 downto 100
+           do (cl-loop for j from i downto 100
+                       for product = (* i j)
+                       while (> product max-palindrome)
+                       when (= product (pe004--reverse-number product))
+                       do (setq max-palindrome product))
+           finally return max-palindrome))
 
 (defun project-euler-4--traditional ()
   "Solve Project Euler 4. 
 Traditional Emacs Lisp without cl-lib"
-  (let ((a 100)
-        (c-max 0))
-    (while (<= a 998)
-      (let ((b a)
-            (c -1))
-        (while (<= b 999)
-          (let ((prod (* a b)))
-            (when (and (= prod (my/reverse-number prod)) (> prod c))
-              (setq c prod)))
-          (setq b (1+ b)))
-        (when (> c c-max)
-          (setq c-max c)))
-      (setq a (1+ a)))
-    c-max))
+  (let ((max-palindrome 0)
+        (i 999))
+    (while (>= i 100)
+      (let ((j i))
+        (while (>= j 100)
+          (let ((product (* i j)))
+            (if (<= product max-palindrome)
+                (setq j 99)  ; Break out of inner loop
+              (when (= product (pe004--reverse-number product))
+                (setq max-palindrome product))))
+          (setq j (1- j))))
+      (setq i (1- i)))
+    max-palindrome))
 
 (ert-deftest test-project-euler-4 ()
   :tags '(elisp-problem-solving)
