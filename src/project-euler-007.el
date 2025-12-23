e2295f0d1e9d46d83788ccbb316b135cd96a94dd
Nicolas O <noccis@I7413>
2025-12-23 15:53:44 +0100

    Improve solution to Project Euler 7

--- src/project-euler-007.el
+++ src/project-euler-007.el
@@ -1,5 +1,13 @@
 ;;; -*- lexical-binding: t; -*-
 
+;;; Table of contents:
+;;;   1) cl-xxx version
+;;;   2) traditional version
+
+
+;;; 1) cl-xxx version
+;;; -----------------
+
 (defun pe007--primep (n)
   "Return t if and only if N is prime. N is supposed to be an integer >= 1.
 (v1, available in occisn/elisp-utils GitHub repository)"
@@ -13,14 +21,37 @@
 		    never (or (zerop (mod n factor))
 			      (zerop (mod n (+ factor 2))))))))
 
+(defun pe007--next-prime (n)
+  "Return next prime after fixnum N.
+Note: if N is prime, the result is not N.
+(v1, available in occisn/elisp-utils GitHub repository)"
+  (if (<= n 1)
+      2
+    (cl-loop for i from (+ n (if (cl-evenp n) 1 2)) by 2 
+             when (pe007--primep i) return i)))
+
+(defun pe007--nth-prime (rank)
+  "Return RANK-th prime.
+For instance: 1 --> 2 ; 2 --> 3 ; 6 --> 13.
+The argument is supposed to be an integer >= 1.
+(v1, available in occisn/elisp-utils GitHub repository)"
+  (cl-loop for n = 2 then (pe007--next-prime n)
+           and count = 1 then (+ count 1)
+           when (= count rank) return n))
+
 (defun project-euler-7 (&optional target0)
   "Solve Project Euler 7."
   (let ((target (or target0 10001)))
-    (cl-loop for n from 2 
-	     when (pe007--primep n)
-	     count t into counter
-	     until (= counter target)
-	     finally return n)))
+    (pe007--nth-prime target)))
+
+(ert-deftest test-project-euler-7 ()
+  :tags '(elisp-problem-solving)
+  (should (= 13 (project-euler-7 6)))
+  (should (= 104743 (project-euler-7))))
+
+
+;;; 2) traditional version
+;;; ----------------------
 
 (defun pe007--isqrt--traditional (n)
   "Return the integer square root of N (largest integer <= sqrt(N)).
@@ -59,24 +90,37 @@ Requires pe007--isqrt--traditional.
                (setq factor (+ factor 6))) ; end of while
              result))))
 
+(defun pe007--next-prime--traditional (n)
+  "Return next prime after fixnum N.
+Note: if N is prime, the result is not N.
+(v1, available in occisn/elisp-utils GitHub repository)"
+  (if (<= n 1)
+      2
+      (let ((i (+ n (if (zerop (mod n 2)) 1 2))))
+        (while (not (pe007--primep--traditional i))
+          (setq i (+ i 2)))
+        i)))
+
+(defun pe007--nth-prime--traditional (rank)
+  "Return RANK-th prime.
+For instance: 1 --> 2 ; 2 --> 3 ; 6 --> 13.
+The argument is supposed to be an integer >= 1.
+(v1, available in occisn/elisp-utils GitHub repository)"
+  (let ((n 2)
+        (count 1))
+    (while (< count rank)
+      (setq n (pe007--next-prime--traditional n))
+      (setq count (1+ count)))
+    n))
+
+
 (defun project-euler-7--traditional ()
   "Solve Project Euler 7.
 Traditional Emacs Lisp, without cl-lib"
-  (let ((n 2)
-        (counter 1) ; 2 is first prime
-        (continue t))
-    (while continue
-      (setq n (1+ n))
-      (when (pe007--primep--traditional n)
-        (setq counter (1+ counter))
-        (when (= counter 10001)
-          (setq continue nil))))
-    n))
+  (pe007--nth-prime--traditional 10001))
 
-(ert-deftest test-project-euler-7 ()
+(ert-deftest test-project-euler-7--traditional ()
   :tags '(elisp-problem-solving)
-  (should (= 13 (project-euler-7 6)))
-  (should (= 104743 (project-euler-7)))
   (should (= 104743 (project-euler-7--traditional))))
 
 ;;; end
